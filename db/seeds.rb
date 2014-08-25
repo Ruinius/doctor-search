53ec7a334fc58d63a82b2f289f2496317e94095a
randallreedjr <randallreedjr@gmail.com>
2014-08-25 09:32:50 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Doctor.create({first_name: 'Nina', 
+               last_name: 'Fallick', 
+               street: '241 East 86th St',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10028',
+               phone: '212-426-0190',
+               specialty: 'Primary Care'})
+
+Doctor.create({first_name: 'Ricardo', 
+               last_name: 'Issa', 
+               street: '157 E 72nd St',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10021',
+               phone: '212-772-1211',
+               specialty: 'Primary Care'})
+
+Doctor.create({first_name: 'Ronald', 
+               last_name: 'Primas', 
+               street: '952 Fifth Avenue',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10075',
+               phone: '212-737-1212',
+               specialty: 'Primary Care'})
+
+Doctor.create({first_name: 'Harry', 
+               last_name: 'Gruenspan', 
+               street: '1317 3rd Ave',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10021',
+               phone: '212-794-2900',
+               specialty: 'Primary Care'})
+
+Doctor.create({first_name: 'Lynn', 
+               last_name: 'Ratner', 
+               street: '112 East 83rd St',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10028',
+               phone: '212-396-0400',
+               specialty: 'Primary Care'})
