653060c7d1f9b8ef8c4ee7e113e44734821d5a0e
randallreedjr <randallreedjr@gmail.com>
2014-08-26 10:41:55 -0400

    Add geocoding to Doctor model, and method to retrieve full address

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,12 @@
 class Doctor < ActiveRecord::Base
+  geocoded_by :full_address
+  after_validation :geocode
+  
+  def full_address
+    "#{street_address}, #{city}, #{state} #{zip}"
+  end
+
+  def street_address
+    unit.blank? ? street : "#{street} #{unit}"
+  end
 end
