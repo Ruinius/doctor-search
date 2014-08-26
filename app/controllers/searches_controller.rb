07044fde17bb4bedf6be475f0c982706bab73672
randallreedjr <randallreedjr@gmail.com>
2014-08-26 11:08:24 -0400

    Choose address and call method to find nearby doctors

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,15 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @doctors = Doctor.all
+    if !params[:address].blank?
+      search = Search.new(params[:address])
+    elsif !params[:zipcode].blank?
+      search = Search.new(params[:zipcode])
+    else
+      redirect_to search_path
+    end
+    @doctors = search.nearby_doctors
     render 'show'
   end
+
 end
