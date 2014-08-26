9d3862d042e1d92c30f9b8ef572dd720ef09326a
randallreedjr <randallreedjr@gmail.com>
2014-08-26 11:08:44 -0400

    Create method to return doctors ranked by distance from search address

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,14 @@
-class Search < ActiveRecord::Base
+class Search 
+  def initialize(address)
+    @address = address
+  end
 
+  def nearby_doctors()
+    # Doctors within 5 miles
+    Doctor.near(self.class.coordinates(@address), 5).sort_by {|d| d.distance}
+  end
+
+  def self.coordinates(address)
+    Geocoder.coordinates(address)
+  end
 end
