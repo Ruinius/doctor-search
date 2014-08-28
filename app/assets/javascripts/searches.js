14b78394af1676c6fe657262a809722dad8c97cd
randallreedjr <randallreedjr@gmail.com>
2014-08-27 21:10:57 -0400

    Display message when no results are returned

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -23,26 +23,30 @@ $( document ).ready(function(){
       dataType: 'script',
       success: function(d){
         var response = JSON.parse(d);
-        if (response[0].hasOwnProperty("notice")){
+        if (response.length == 0) {
+          $('.no_results').remove();
+          $('div.container').prepend("<p class=no_results><strong>No results found in your search area.</strong></p>");
+        } else if (response[0].hasOwnProperty("notice")){
+          $('.notice').remove();
           $('div.container').prepend("<p class=notice>" + response[0]["notice"]);
         } else {
+          $('.no_results').remove();
           $('.notice').remove();
           $form.hide();
           var $title = $('h2');
-          $title.html("Search results")
-
+          $title.html("Search results");
           var $results = $('#doctor_results > ol');
           $results.html("");
-          var limit = Math.min(10, response.length)
+          var limit = Math.min(10, response.length);
           for (var i = 0; i < limit; i++){
             $results.append("<li>" + response[i]["last_name"] + ", " + response[i]["first_name"] + "</li>");
-            $results.append("<p>Specialty: " + response[i]["specialties"])
+            $results.append("<p>Specialty: " + response[i]["specialties"]);
             $results.append("<p>" + response[i]["full_address"] + "</p>");
             $results.append("<p>Distance: " + response[i]["distance"] + " miles</p>");
           }
           
           if (response.length > 10 && $form.find('#specialty_id').val() == ""){
-            $('#doctor_results').prepend("<strong>More than 10 results were returned. Try selecting a specialty to narrow the results.</strong>")
+            $('#doctor_results').prepend("<p><strong>More than 10 results were returned. Try selecting a specialty to narrow the results.</strong></p>");
           }
           $('#doctor_results').append("<a id=search_again href='/'>");
           $('#doctor_results > a').text("Search again");
