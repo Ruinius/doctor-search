43fb115d28af2c07f99344c93a02d12b5358fd9a
randallreedjr <randallreedjr@gmail.com>
2014-08-28 08:57:36 -0400

    Add method to searches controller to create JSON object for doctors list

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,22 +8,26 @@ class SearchesController < ApplicationController
     if params[:address].blank? && params[:zipcode].blank?
       render :json => [{:notice => "Please enter an address"}]
     else
-      search = Search.new(search_params)
-      doctors_json = search.nearby_doctors.collect do |doctor|
-        {
-          :first_name => doctor.first_name,
-          :last_name => doctor.last_name,
-          :full_address => doctor.full_address,
-          :distance => doctor.distance.round(2),
-          :specialties => doctor.specialty_list
-        }
-      end
       render :json => doctors_json
     end
   end
 
+  private
   def search_params
     params.permit(:address,:zipcode).merge(params.require(:specialty).permit(:id))
+  end
+
+  def doctors_json()
+    search = Search.new(search_params)
+    search.nearby_doctors.collect do |doctor|
+      {
+        :first_name => doctor.first_name,
+        :last_name => doctor.last_name,
+        :full_address => doctor.full_address,
+        :distance => doctor.distance.round(2),
+        :specialties => doctor.specialty_list
+      }
+    end
   end
 
 end
