98331c96d9b197d186713687014762aeaadeda2d
randallreedjr <randallreedjr@gmail.com>
2014-08-27 20:57:27 -0400

    Generate enough doctors to trigger results limit warning

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,6 +52,51 @@ doctors << Doctor.create({first_name: 'Lynn',
                zip: '10028',
                phone: '212-396-0400'})
 
+doctors << Doctor.create({first_name: 'Orit', 
+               last_name: 'Beitner', 
+               street: '211 Central Park West',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10024',
+               phone: '212-877-3210'})
+
+doctors << Doctor.create({first_name: 'Robert', 
+               last_name: 'Romanoff', 
+               street: '115 Central Park West',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10023',
+               phone: '212-866-2100'})
+
+doctors << Doctor.create({first_name: 'Athanasios', 
+               last_name: 'Mallios', 
+               street: '15 West 82nd St',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10023',
+               phone: '212-787-0091'})
+
+doctors << Doctor.create({first_name: 'Richard', 
+               last_name: 'Shepard', 
+               street: '140 West 69th St',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10023',
+               phone: '212-496-9620'})
+
+doctors << Doctor.create({first_name: 'Haleh', 
+               last_name: 'Milani', 
+               street: '115 Central Park West',
+               unit: '',
+               city: 'New York',
+               state: 'NY',
+               zip: '10463',
+               phone: '212-543-3400'})
+
 doctors << Doctor.create({first_name: 'Gillian', 
                last_name: 'Goddard', 
                street: '103 East 75th St',
@@ -62,6 +107,6 @@ doctors << Doctor.create({first_name: 'Gillian',
                phone: '212-772-7628'})
 
 DoctorSpecialty.create({doctor_id: doctors.last.id, specialty_id: specialties.last.id})
-doctors[0..4].each do |doctor|
+doctors[0..-2].each do |doctor|
   DoctorSpecialty.create({doctor_id: doctor.id, specialty_id: specialties.first.id}) 
 end
